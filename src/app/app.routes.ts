8f6494be6b9ff20f6dea1a9fc8343de3d176c551
Marwan <marwanneno1111@gmail.com>
2025-08-02 15:47:41 +0300

    Marwan

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Signup } from './components/signup/signup';
+import { HomeComponent } from './components/Home/home/home.component';
 
 export const routes: Routes = [
-    {path:'login',component:Login},
-    {path:'signup',component:Signup},
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent, title: 'Home' },
+    { path: 'login', component: Login, title: 'Login' },
+    { path: 'signup', component: Signup, title: 'SignUp' },
 ];
